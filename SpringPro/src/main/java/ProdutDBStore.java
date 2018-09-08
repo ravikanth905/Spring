9f46ea170aff6c8556c40ee2de301b8e8855e07b
ravikanth donthula <ravikanth donthula@192.168.0.50>
2018-09-08 01:01:14 -0500

    First Commit

--- SpringPro/src/main/java/ProdutDBStore.java
+++ SpringPro/src/main/java/ProdutDBStore.java
@@ -21,4 +21,3 @@ class ProdutDBStore {
 		return product;
 	}
 }
-
--- SpringPro/src/main/java/Test.java
+++ SpringPro/src/main/java/Test.java
@@ -1,9 +1,19 @@
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		ApplicationContext Context = new ClassPathXmlApplicationContext("bean.xml");
+		OrderService order=Context.getBean("processOrder",OrderService.class);
+
+		order.processOrder(new Order(125, 4, 1));
+		order.processOrder(new Order(126, 8, 3));
+
+	
 
 	}
-
 }
+
